6b2d55fbb550281106c709b1d67dc5f6221c1575
choirobloxkhum <hiendichai@gmail.com>
2026-09-08 21:38:37 +0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/-nghi-n-r-c-bl--c/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
